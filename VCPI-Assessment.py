1e6e7ef1bf0c30e8991707689c1473290c5a1f48
David Lloyd <david.george.lloyd@gmail.com>
2015-09-16 14:48:22 -0700

    Second Update

--- VCPI-Assessment.py
+++ VCPI-Assessment.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter.font import *
 from tkinter.ttk import *
+import wmi
+import datetime
 
 class Assessment(Tk):
     def __init__(self, parent):
@@ -10,6 +12,7 @@ class Assessment(Tk):
 
     def initialize(self):
         self.title('VCPI Assessment')
+        self.wmi = wmi.WMI()
         self.grid()
         self.font = Font(family='New Time Roman', size=9)
 
@@ -43,7 +46,62 @@ class Assessment(Tk):
         self.label_four = Label(self, text='Comment', font=self.font)
         self.label_four.grid(column=0, row=3, sticky='W')
 
+    def get_os_name(self):
+        for os in self.wmi.Win32_OperatingSystem():
+            return os.caption
+
+    def get_os_version(self):
+        for os in self.wmi.Win32_OperatingSystem():
+            return os.version
+
+    def get_manufacturer(self):
+        for computer in self.wmi.Win32_ComputerSystem():
+            return computer.manufacturer
+
+    def get_name(self):
+        for computer in self.wmi.Win32_ComputerSystem():
+            return computer.name
+
+    def get_model(self):
+        for computer in self.wmi.Win32_ComputerSystem():
+            return computer.model
+
+    def get_install_date(self):
+        for os in self.wmi.Win32_OperatingSystem():
+            return str(datetime.datetime.strptime(str(os.installdate)[0:8], '%Y%m%d')).split()[0]
+
+    def get_architecture(self):
+        for os in self.wmi.Win32_OperatingSystem():
+            return os.osarchitecture
+
+    def get_domain(self):
+        for computer in self.wmi.Win32_ComputerSystem():
+            return computer.domain
+
+    def get_processor(self):
+        for processor in self.wmi.Win32_Processor():
+            return processor.name
+
+    def get_memory(self):
+        for os in self.wmi.Win32_OperatingSystem():
+            return os.totalvisiblememorysize
+
+    def get_serial(self):
+        for bios in self.wmi.Win32_Bios():
+            return bios.serialnumber
+
+    def get_network_address(self):
+        return self.wmi.Win32_NetworkAdapterConfiguration()[1].ipaddress[0]
+
 if __name__ == "__main__":
     app = Assessment(None)
-    app.set_font_size(5)
+    print(app.get_os_name())
+    print(app.get_os_version())
+    print(app.get_name())
+    print(app.get_model())
+    print(app.get_architecture())
+    print(app.get_domain())
+    print(app.get_network_address())
+    print(app.get_processor())
+    print(app.get_install_date())
     app.mainloop()
