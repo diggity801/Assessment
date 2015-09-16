fd12a974946ab967ceda173ca4a7f791dfcaf227
David Lloyd <david.george.lloyd@gmail.com>
2015-09-16 12:16:25 -0700

    Update

--- VCPI-Assessment.py
+++ VCPI-Assessment.py
@@ -14,16 +14,16 @@ class Assessment(Tk):
         self.font = Font(family='New Time Roman', size=9)
 
         self.entry_one = Entry(self, font=self.font)
-        self.entry_one.grid(column=0, row=0)
+        self.entry_one.grid(column=1, row=0, sticky='E')
 
         self.entry_two = Entry(self, font=self.font)
-        self.entry_two.grid(column=0, row=1)
+        self.entry_two.grid(column=1, row=1, sticky='E')
 
         self.entry_three = Entry(self, font=self.font)
-        self.entry_three.grid(column=0, row=2)
+        self.entry_three.grid(column=1, row=2, sticky='E')
 
         self.entry_four = Entry(self, font=self.font)
-        self.entry_four.grid(column=0, row=3)
+        self.entry_four.grid(column=1, row=3, sticky='E')
 
         self.button_one = tkinter.Button(self, text='Query', font=self.font)
         self.button_one.grid(column=0, row=4, sticky='W')
@@ -31,6 +31,19 @@ class Assessment(Tk):
         self.button_two = tkinter.Button(self, text='Save',font=self.font)
         self.button_two.grid(column=0, row=5, sticky='W')
 
+        self.label_one = Label(self, text='Facility ID', font=self.font)
+        self.label_one.grid(column=0, row=0, sticky='W')
+
+        self.label_two = Label(self, text='PC ID', font=self.font)
+        self.label_two.grid(column=0, row=1, sticky='W')
+
+        self.label_three = Label(self, text='Location', font=self.font)
+        self.label_three.grid(column=0, row=2, sticky='W')
+
+        self.label_four = Label(self, text='Comment', font=self.font)
+        self.label_four.grid(column=0, row=3, sticky='W')
+
 if __name__ == "__main__":
     app = Assessment(None)
+    app.set_font_size(5)
     app.mainloop()
